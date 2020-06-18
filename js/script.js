ecf771e1d1a83bd66f77ce8f96d02823ca595c6b
Osama Gadit <ogadit@gmail.com>
2020-06-19 03:55:46 +0500

    1. Removed the menu 2. Added functionality to change method and school

--- js/script.js
+++ js/script.js
@@ -4,7 +4,27 @@ var app = new Vue({
     city: "",
     country: "",
     method: 1,
+    methodList: [
+      "Shia Ithna-Ansar",
+      "University of Islamic Sciences, Karachi",
+      "Islamic Society of North America",
+      "Muslim World League",
+      "Umm Al-Qura University, Makkah",
+      "Egyptian General Authority of Survey",
+      "Institute of Geophysics, University of Tehran",
+      "Gulf Region",
+      "Kuwait",
+      "Qatar",
+      "Majlis Ugama Islam Singapura, Singapore",
+      "Union Organization islamic de France",
+      "Diyanet İşleri Başkanlığı, Turkey",
+      "Spiritual Administration of Muslims of Russia",
+    ],
     school: 1,
+    schoolList: [
+      "Shafa'i",
+      "Hanafi"
+    ],
     times: {},
     date: "",
     showChangeCityModal: false,
@@ -20,26 +40,24 @@ var app = new Vue({
     refreshData: function () {
       axios
         .get(
-          `https://api.aladhan.com/v1/timingsByCity?city=${this.city}&country=${this.country}&method=1&school=1`
+          `https://api.aladhan.com/v1/timingsByCity?city=${this.city}&country=${this.country}&method=${this.method}&school=${this.school}`
         )
         .then((res) => {
-          if(res.code != 400) {
+          if (res.code != 400) {
             this.times = res.data.data.timings;
             this.date = `${res.data.data.date.gregorian.weekday.en}, ${res.data.data.date.hijri.day} ${res.data.data.date.hijri.month.en} ${res.data.data.date.hijri.year}AH`;
             this.showModal(false);
-            document.getElementById("check").checked = false
-          } else { 
-              this.date = "Invalid"
+          } else {
+            this.date = "Invalid";
           }
         });
-      
     },
   },
   beforeMount() {
     axios.get(`https://ipapi.co/json/`).then((a) => {
-        this.city = a.data.city
-        this.country = a.data.country_name
-        this.refreshData()
-    })
+      this.city = a.data.city;
+      this.country = a.data.country_name;
+      this.refreshData();
+    });
   },
 });
